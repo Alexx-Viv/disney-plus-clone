524ff44e9325312bb68c47be79bf1115ceb5014c
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2022-02-01 17:35:14 +0100

    add styled-components init header and home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import './App.css'
+import Header from './components/Header'
+import Home from './components/Home'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <Header />
+      {/* <Home /> */}
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+  return (
+    <Nav>
+      <Logo src='' />
+      <NavMenu></NavMenu>
+    </Nav>
+  )
+}
+
+export default Header
+
+const Nav = styled.nav`
+  height: 70px;
+  background-color: #090b13;
+`
+const Logo = styled.image`
+  width: 80px;
+`
+const NavMenu = styled.div``
